9f0a3ea693b764e8476bec0e5c1779cbef815dc2
Artur Lomsadze <lomsadze@bpcbt.com>
2017-02-22 13:24:30 +0300

    Add camel tests

---
+++ src/test/java/com/bpcbt/sv/camel/RouterTest.java
@@ -0,0 +1,107 @@
+package com.bpcbt.sv.camel;
+
+import javax.jms.ConnectionFactory;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.camel.CamelContext;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.jms.JmsComponent;
+import org.apache.camel.component.wmq.WmqComponent;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.junit.Test;
+
+public class RouterTest {
+
+	private static final String HOSTNAME = "mint";
+	private static final int PORT = 1414;
+	private static final String QUEUE_MANAGER = "MQTest";
+	private static final String CHANNEL = "HPT5.CLNT.WL";
+	private static final String MQ_PASS = "mquser";
+	private static final String MQ_USER = MQ_PASS;
+
+	@Test
+	public void testCamel() throws Exception {
+		CamelContext context = new DefaultCamelContext();
+
+		WmqComponent wmqComponent = WmqComponent.newWmqComponent(HOSTNAME, PORT, QUEUE_MANAGER, CHANNEL).excludeRFHeaders();
+		context.addComponent("wmq", wmqComponent);
+
+		context.addRoutes(new RouteBuilder() {
+			@Override
+			public void configure() throws Exception {
+				from("wmq:MQTestQueue?username=" + MQ_USER + "&password=" + MQ_PASS)
+						.to("stream:out");
+			}
+		});
+		context.start();
+	}
+
+	//	@Override
+	//	protected CamelContext createCamelContext() throws Exception {
+	//		CamelContext context = super.createCamelContext();
+	//		context.addComponent("wmq", WmqComponent.newWmqComponent("mint", 1414, "MQTest", "HPT5.CLNT.WL")
+	//				.excludeRFHeaders());
+	//		return context;
+	//	}
+
+	@Test
+	public void send_and_receive() throws Exception {
+		//		template().sendBodyAndHeader("wmq:MQTestQueue?username=mquser&password=mquser", "aMessage", "JMS_IBM_MQMD_ApplIdentityData", "anyIdData");
+		//
+		//		Exchange received = consumer().receive("wmq:MQTestQueue?username=mquser&password=mquser");
+		//		Message in = received.getIn();
+		//
+		//		assertEquals("aMessage", in.getBody());
+		//		assertStartsWith(in.getHeader("JMS_IBM_MQMD_ApplIdentityData").toString(), "anyIdData");
+	}
+
+	//	private void assertStartsWith(String string, String starts) {
+	//		assertTrue(string.startsWith(starts));
+	//	}
+
+	@Test
+	public void testCamel2() throws Exception {
+		// START SNIPPET: e1
+		CamelContext context = new DefaultCamelContext();
+		// END SNIPPET: e1
+		// Set up the ActiveMQ JMS Components
+		// START SNIPPET: e2
+		ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("vm://localhost?broker.persistent=false");
+		// Note we can explicit name the component
+		context.addComponent("test-jms", JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
+		// END SNIPPET: e2
+		// Add some configuration by hand ...
+		// START SNIPPET: e3
+		context.addRoutes(new RouteBuilder() {
+			public void configure() {
+				from("test-jms:queue:test.queue").to("file://target/testRoute");
+			}
+		});
+		// END SNIPPET: e3
+		// Camel template - a handy class for kicking off exchanges
+		// START SNIPPET: e4
+		ProducerTemplate template = context.createProducerTemplate();
+		// END SNIPPET: e4
+		// Now everything is set up - lets start the context
+		context.start();
+		// Now send some test text to a component - for this case a JMS Queue
+		// The text get converted to JMS messages - and sent to the Queue
+		// test.queue
+		// The file component is listening for messages from the Queue
+		// test.queue, consumes
+		// them and stores them to disk. The content of each file will be the
+		// test we sent here.
+		// The listener on the file component gets notified when new files are
+		// found ... that's it!
+		// START SNIPPET: e5
+		for (int i = 0; i < 10; i++) {
+			template.sendBody("test-jms:queue:test.queue", "Test Message: " + i);
+		}
+		// END SNIPPET: e5
+
+		// wait a bit and then stop
+		Thread.sleep(1000);
+		context.stop();
+	}
+}
